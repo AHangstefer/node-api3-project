d838641022a7b85168a0d5a17b4b0d01707ceb80
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-10-07 14:30:08 -0400

    setting up server

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const postRouter = require("./posts/postRouter")
-const userRouter = requre("./users/userRouter")
+const userRouter = require("./users/userRouter")
+const morgan = require("morgan")
 
 const server = express();
+const port = 3000
 
 //custom middleware
 function logger(req, res, next) {
@@ -18,6 +20,7 @@ server.use(logger);
 server.use(express.json());
 server.use('/api/posts', postRouter);
 server.use('/api/users', userRouter);
+server.use(morgan("combined"))
 
 
 
@@ -27,6 +30,10 @@ server.get('/', (req, res) => {
     message: `This is my custom middleware from server ${process.env.COHORT}`,
     fact: process.env.FUN_FACT || "most people think the sky is blue",
   })
+});
+
+server.listen(port, ()=> {
+  console.log(`Server running at http://localhost:${port}`)
 });
 
 module.exports = server;
