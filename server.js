c50b9b3bcec151ad2d04e9c43223f5b29cdb695a
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2020-10-06 22:19:25 -0400

    added server

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const postRouter = require("./posts/postRouter")
+const userRouter = requre("./users/userRouter")
 
 const server = express();
 
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
-
 //custom middleware
+function logger(req, res, next) {
+  const date = new Date().toString();
+  const url = req.url;
+  const method = req.method;
+  console.log(`${date} -- ${method} -- ${url}`);
 
-function logger(req, res, next) {}
+  next();
+
+}
+server.use(logger);
+server.use(express.json());
+server.use('/api/posts', postRouter);
+server.use('/api/users', userRouter);
+
+
+
+
+server.get('/', (req, res) => {
+  res.status(200).json({
+    message: `This is my custom middleware from server ${process.env.COHORT}`,
+    fact: process.env.FUN_FACT || "most people think the sky is blue",
+  })
+});
 
 module.exports = server;
